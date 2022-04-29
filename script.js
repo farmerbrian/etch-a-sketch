ccce928dfd1996c64b7f6004b7d3147f90e014e4
farmerbrian <nichols.brian.d@gmail.com>
2022-04-29 09:57:24 -0500

    Code Cleanup

--- script.js
+++ script.js
@@ -16,10 +16,6 @@ btn.addEventListener('click', () => {
     
 });
 
-//let squareSelection;
-//let squares;
-// let changeColor;
-
 function drawGrid(rows,columns) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', columns);
@@ -52,17 +48,3 @@ function drawGrid(rows,columns) {
 
 let pageLoad = drawGrid(16,16);
 
-// const buttons = document.querySelectorAll('button');
-//     buttons.forEach((button) => {
-//     // and for each one we add a 'click' listener
-//     button.addEventListener('click', () => {
-//     playerSelection = (button.id);
-
-// const gridRow = document.createElement('div');
-// gridRow.classList.add('gridRow');
-// gridRow.style.border = '2px';
-// gridRow.style.borderStyle = 'solid';
-// gridRow.style.borderColor = 'black';
-// gridRow.style.gap = '0px';
-
-// container.appendChild(gridRow);
