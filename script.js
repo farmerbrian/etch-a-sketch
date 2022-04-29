1794d0c5f65629303bc24e7cb1c8f3bb9b2256d8
farmerbrian <nichols.brian.d@gmail.com>
2022-04-29 08:29:09 -0500

    Added input limit and refactored code

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
 const container = document.querySelector('#container');
-
 const btn = document.querySelector('button');
 let gridSize;
 
 
 btn.addEventListener('click', () => {
-    gridSize = prompt('How big do you want your grid?', 16);
-    drawGrid(gridSize,gridSize);
+    gridSize = prompt('How big do you want your grid? 50 or less', 16);
+    if (gridSize > 50) {
+        gridSize = prompt('Try again but make it 50 or less this time', 16);
+        if (gridSize < 50){
+            drawGrid(gridSize,gridSize)
+        }
+    } else {
+        drawGrid(gridSize,gridSize)
+    }
+    
 });
 
-let squareSelection;
-let squares;
-let changeColor;
+//let squareSelection;
+//let squares;
+// let changeColor;
 
 function drawGrid(rows,columns) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', columns);
     for (c = 0; c < (rows * columns); c++) {
         let cell = document.createElement("div");
-        //cell.innerText = (c + 1);
         container.appendChild(cell).className = 'grid-item';
         container.appendChild(cell).id = `cell-${c}`;
         
-        squares = document.querySelectorAll('.grid-item');
+        let squares = document.querySelectorAll('.grid-item');
             squares.forEach((item) => {
                 item.className = 'grid-item';
                 item.addEventListener('click', () => {
-                    squareSelection = (item.id);
-                    changeColor = document.getElementById(`${squareSelection}`);
+                    let squareSelection = (item.id);
+                    let changeColor = document.getElementById(`${squareSelection}`);
                     changeColor.className = 'grid-item item-color';
                 })
                 
                 item.addEventListener('mouseenter', () => {
                     hover = (item.id);
                     showHover = document.getElementById(`${hover}`).classList.add('hover-color');
-                    //showHover.className = 'grid-item hover-color';
                 })
                 item.addEventListener('mouseleave', () => {
                     hover = (item.id);
                     showHover = document.getElementById(`${hover}`).classList.remove('hover-color');
-                    //showHover.className = 'grid-item';
                 })
             }) 
     };
